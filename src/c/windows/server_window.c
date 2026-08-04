19eb3ae4cdaaf74c7283f1f4b9ef758786d88535
AndrewK <akism90@gmail.com>
2026-08-04 11:18:12 +0200

    Refactor UI into reusable widgets and complete server dashboard mock

--- src/c/windows/server_window.c
+++ src/c/windows/server_window.c
@@ -3,48 +3,77 @@
 #include <stdio.h>
 
 #include "../services/server_status_service.h"
+#include "../ui/metric_row.h"
 #include "../ui/status_row.h"
 
-#define SERVER_CONTENT_HEIGHT 380
+#define SERVER_METRIC_COUNT 5
+#define SERVER_CONTENT_HEIGHT 440
+
+typedef enum
+{
+    SERVER_METRIC_CPU = 0,
+    SERVER_METRIC_RAM,
+    SERVER_METRIC_TEMPERATURE,
+    SERVER_METRIC_LOAD,
+    SERVER_METRIC_UPTIME
+} ServerMetric;
 
 static ScrollLayer *s_scroll_layer;
 static TextLayer *s_server_title_layer;
-static TextLayer *s_metrics_layer;
 static TextLayer *s_services_title_layer;
 static TextLayer *s_updated_layer;
 
 static StatusRow *s_server_status_row;
 static StatusRow *s_service_rows[SERVER_SERVICE_COUNT];
+static MetricRow *s_metric_rows[SERVER_METRIC_COUNT];
 
-static char s_metrics_text[160];
+static char s_cpu_text[16];
+static char s_ram_text[16];
+static char s_temperature_text[16];
+static char s_load_text[16];
 static char s_updated_text[48];
 
-static void prv_format_metrics(const ServerStatus *status)
+static void prv_format_metric_values(const ServerStatus *status)
 {
-    const int cpu_tenths = (int)(status->cpu_percent * 10.0f);
-    const int ram_tenths = (int)(status->ram_percent * 10.0f);
+    const int cpu_tenths =
+        (int)(status->cpu_percent * 10.0f);
+    const int ram_tenths =
+        (int)(status->ram_percent * 10.0f);
     const int temperature_tenths =
         (int)(status->temperature_celsius * 10.0f);
     const int load_hundredths =
         (int)(status->load_average * 100.0f);
 
     snprintf(
-        s_metrics_text,
-        sizeof(s_metrics_text),
-        "CPU        %d.%d%%\n"
-        "RAM       %d.%d%%\n"
-        "Temp      %d.%d C\n"
-        "Load       %d.%02d\n"
-        "Uptime    %s",
+        s_cpu_text,
+        sizeof(s_cpu_text),
+        "%d.%d%%",
         cpu_tenths / 10,
-        cpu_tenths % 10,
+        cpu_tenths % 10
+    );
+
+    snprintf(
+        s_ram_text,
+        sizeof(s_ram_text),
+        "%d.%d%%",
         ram_tenths / 10,
-        ram_tenths % 10,
+        ram_tenths % 10
+    );
+
+    snprintf(
+        s_temperature_text,
+        sizeof(s_temperature_text),
+        "%d.%d C",
         temperature_tenths / 10,
-        temperature_tenths % 10,
+        temperature_tenths % 10
+    );
+
+    snprintf(
+        s_load_text,
+        sizeof(s_load_text),
+        "%d.%02d",
         load_hundredths / 100,
-        load_hundredths % 100,
-        status->uptime_text
+        load_hundredths % 100
     );
 }
 
@@ -61,7 +90,7 @@ static void prv_window_load(Window *window)
     const GRect bounds = layer_get_bounds(window_layer);
     const ServerStatus status = server_status_service_get();
 
-    prv_format_metrics(&status);
+    prv_format_metric_values(&status);
 
     snprintf(
         s_updated_text,
@@ -112,27 +141,38 @@ static void prv_window_load(Window *window)
         true
     );
 
-    s_metrics_layer = text_layer_create(
-        GRect(12, 72, bounds.size.w - 24, 110)
+    s_metric_rows[SERVER_METRIC_CPU] = metric_row_create(
+        GRect(12, 72, bounds.size.w - 24, 24),
+        "CPU",
+        s_cpu_text
     );
 
-    text_layer_set_text(
-        s_metrics_layer,
-        s_metrics_text
+    s_metric_rows[SERVER_METRIC_RAM] = metric_row_create(
+        GRect(12, 96, bounds.size.w - 24, 24),
+        "RAM",
+        s_ram_text
     );
 
-    text_layer_set_font(
-        s_metrics_layer,
-        fonts_get_system_font(FONT_KEY_GOTHIC_18)
+    s_metric_rows[SERVER_METRIC_TEMPERATURE] = metric_row_create(
+        GRect(12, 120, bounds.size.w - 24, 24),
+        "Temp",
+        s_temperature_text
     );
 
-    text_layer_set_text_alignment(
-        s_metrics_layer,
-        GTextAlignmentLeft
+    s_metric_rows[SERVER_METRIC_LOAD] = metric_row_create(
+        GRect(12, 144, bounds.size.w - 24, 24),
+        "Load",
+        s_load_text
+    );
+
+    s_metric_rows[SERVER_METRIC_UPTIME] = metric_row_create(
+        GRect(12, 168, bounds.size.w - 24, 24),
+        "Uptime",
+        status.uptime_text
     );
 
     s_services_title_layer = text_layer_create(
-        GRect(0, 184, bounds.size.w, 30)
+        GRect(0, 202, bounds.size.w, 30)
     );
 
     text_layer_set_text(
@@ -156,7 +196,7 @@ static void prv_window_load(Window *window)
         s_service_rows[index] = status_row_create(
             GRect(
                 12,
-                218 + (index * 24),
+                236 + (index * 24),
                 bounds.size.w - 24,
                 24
             ),
@@ -166,7 +206,7 @@ static void prv_window_load(Window *window)
     }
 
     s_updated_layer = text_layer_create(
-        GRect(12, 364, bounds.size.w - 24, 40)
+        GRect(12, 392, bounds.size.w - 24, 40)
     );
 
     text_layer_set_text(
@@ -196,10 +236,16 @@ static void prv_window_load(Window *window)
         );
     }
 
-    scroll_layer_add_child(
-        s_scroll_layer,
-        text_layer_get_layer(s_metrics_layer)
-    );
+    for (int index = 0;
+         index < SERVER_METRIC_COUNT;
+         ++index) {
+        if (s_metric_rows[index] != NULL) {
+            scroll_layer_add_child(
+                s_scroll_layer,
+                metric_row_get_layer(s_metric_rows[index])
+            );
+        }
+    }
 
     scroll_layer_add_child(
         s_scroll_layer,
@@ -243,8 +289,12 @@ static void prv_window_unload(Window *window)
     text_layer_destroy(s_services_title_layer);
     s_services_title_layer = NULL;
 
-    text_layer_destroy(s_metrics_layer);
-    s_metrics_layer = NULL;
+    for (int index = 0;
+         index < SERVER_METRIC_COUNT;
+         ++index) {
+        metric_row_destroy(s_metric_rows[index]);
+        s_metric_rows[index] = NULL;
+    }
 
     status_row_destroy(s_server_status_row);
     s_server_status_row = NULL;
